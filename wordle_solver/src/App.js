577982787e3c1dac4c876a6ffc8f4dc3f2394cbb
Khalil <khalilfoulks@gmail.com>
2022-03-02 15:42:00 -0500

    added component to store possible words

--- wordle_solver/src/App.js
+++ wordle_solver/src/App.js
@@ -1,4 +1,5 @@
 import InputForm from './components/InputForm';
+import PossibleWords from './components/PossibleWords';
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <h1>Wordle Solver</h1>
       <InputForm/>
+      <PossibleWords/>
     </div>
   );
 }
---
+++ wordle_solver/src/components/PossibleWords.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function PossibleWords(){
+    return(
+        <div>
+            <div>
+                <h2>Possible Answers</h2>
+            </div>
+            <div>
+                <h2>Possible Guesses</h2>
+            </div>
+        </div>
+    )
+}
+
+export default PossibleWords
