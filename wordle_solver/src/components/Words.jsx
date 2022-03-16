5e99bf482044fb01a8fd46293739a46a7ca5e169
Khalil <khalilfoulks@gmail.com>
2022-03-16 15:56:38 -0400

    added app styling

--- wordle_solver/src/components/Words.jsx
+++ wordle_solver/src/components/Words.jsx
@@ -3,33 +3,33 @@ import React from "react";
 function Words(props){
     const { suggestedGuesses, possibleWords, isLoading, tooManyAnswers } = props
     return(
-        <div>
+        <div className="words-container">
             {
                 isLoading || possibleWords === [] ? 
-                <div>
-                    <div>
+                <div className="">
+                    <div className="answers">
                         <h2>Possible Answers</h2>
                     </div>
-                    <div>
+                    <div className="suggested-guesses">
                         <h2>Suggested Guesses</h2>
                     </div>
                 </div> :
                 <div>
-                    <div>
+                    <div className="answers">
                         <h2>Possible Answers</h2>
                         {
                             tooManyAnswers ? <div className="too-many-guesses">There are too many possible answers, please limit further</div> : <></> 
                         }       
-                        {possibleWords.map((answers) => (
-                            <div key={answers}>
+                        {possibleWords.map((answers, idx) => (
+                            <div key={idx} className="answers-containter">
                                 {answers}
                             </div>
                         ))}
                     </div>
-                    <div>
+                    <div className="suggested-guesses">
                         <h2>Suggested Guesses</h2>
-                        {suggestedGuesses.map((guesses) => (
-                            <div key={guesses}>
+                        {suggestedGuesses.map((guesses, idx) => (
+                            <div key={idx} className='guesses-containter'>
                                 {guesses}
                             </div>
                         ))}
